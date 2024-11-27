be3ed7255c204895c2de692d568db6c8bae44e6e
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-11-26 22:42:35 -0300

    Cats vc Dogs Dataset

---
+++ transferlearning.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from torchinfo import summary
+import numpy as np
+import os
+import glob
+import pytorchcv
+
+def train(model, dataloader, criterion, optimizer, device):
+    model.train()
+    running_loss = 0.0
+    for inputs, labels in dataloader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    return running_loss / len(dataloader)
+
+def plot_results(train_loss, val_loss, epochs):
+    plt.plot(range(1, epochs + 1), train_loss, label='Training Loss')
+    plt.plot(range(1, epochs + 1), val_loss, label='Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+
+
+def check_image_dir(pattern):
+    images = glob.glob(pattern)
+    if not images:
+        print(f"No images found for pattern: {pattern}")
+    else:
+        print(f"Found {len(images)} images for pattern: {pattern}")
+
+def display_dataset(dataset, num_images=5):
+    fig, axes = plt.subplots(1, num_images, figsize=(15, 5))
+    for i in range(num_images):
+        image, label = dataset[i]
+        axes[i].imshow(image.permute(1, 2, 0))  # Transpor canais para exibição
+        axes[i].set_title(f"Label: {label}")
+        axes[i].axis('off')
+    plt.show()
+
+
+check_image_dir(r'C:\Users\phenr\Downloads\kagglecatsanddogs_5340\PetImages\Dog\*.jpg')
+check_image_dir(r'C:\Users\phenr\Downloads\kagglecatsanddogs_5340\PetImages\Cat\*.jpg')
+
+
+std_normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                          std=[0.229, 0.224, 0.225])
+trans = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(), 
+        std_normalize])
+dataset = torchvision.datasets.ImageFolder(r'C:\Users\phenr\Downloads\kagglecatsanddogs_5340\PetImages',transform=trans)
+trainset, testset = torch.utils.data.random_split(dataset,[20000,len(dataset)-20000])
+
+display_dataset(dataset)
